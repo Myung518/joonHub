228f9bef09572b3b040d5037885a39c04cb1791a
테리귀여워 <75843997+Myung518@users.noreply.github.com>
2023-02-23 22:20:23 +0900

    [Gold IV] Title: 알파벳, Time: 940 ms, Memory: 15336 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,51 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
-class Point {
-	int x, y;
-	
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
 public class Main {
+
 	static int R, C;
 	static char[][] arr;
-	static Set<Character> s = new HashSet<>();
-	static final int STARTX = 0, STARTY = 0;
+	static boolean[] c;
 	static int max = Integer.MIN_VALUE;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		arr = new char[R][];
+		c = new boolean[26];
 		
 		for (int i = 0; i < R; i++) {
 			arr[i] = br.readLine().toCharArray();
 		}
-		s.add(arr[STARTX][STARTY]);
-		f(STARTX, STARTY);
+		c[arr[0][0] - 'A'] = true;
+		f(1, 0, 0);
 		System.out.println(max);
 	}
 	static int dis[][] = { {-1, 0}, {0, -1}, {1, 0}, {0, 1} };
-	public static void f(int x, int y) {
-		max = Math.max(max, s.size());
+	public static void f(int count, int x, int y) {
+		max = Math.max(max, count);
 		for (int[] tmp : dis) {
 			int tx = x + tmp[0];
 			int ty = y + tmp[1];
-			if (tx >= 0 && ty >= 0 && tx < R && ty < C && !s.contains(arr[tx][ty])) {
-				s.add(arr[tx][ty]);
-				f(tx, ty);
-				s.remove(arr[tx][ty]);
+			if (tx >= 0 && ty >= 0 && tx < R && ty < C && !c[arr[tx][ty] - 'A']) {
+				c[arr[tx][ty] - 'A'] = true;
+				f(count + 1, tx, ty);
+				c[arr[tx][ty] - 'A'] = false;
 			}
 		}
 	}
